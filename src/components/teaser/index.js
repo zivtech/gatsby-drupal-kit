74bbd3fb59125bfafde4363728b1e6ea931c37d9
Nicholas Lewis <nicklewisatx@gmail.com>
2018-08-20 17:14:32 -0400

    Updating some text

--- src/components/teaser/index.js
+++ src/components/teaser/index.js
@@ -1,32 +1,26 @@
 import React from "react";
 import styles from "./teaser.module.css";
+console.log(styles);
 import Img from "gatsby-image";
 import Container from "../container";
 import { rhythm } from "../../utils/typography"
 
 class Teaser extends React.Component {
   render() {
-
     return (
       <div
         css={{
           margin: '1em 0',
           marginBottom: rhythm(2),
           clear: 'both'
-        }}>
-        <div
-          css={{
-            float: 'left',
-            width: '200px',
-
-          }}
-        >
+        }} className={styles.teaser}>
+        <div className={styles.imageWrapper}> 
           <Img
             fluid={
               this.props.image
             } 
           />
-        </div>
+     </div>
         <h3><a href={this.props.path}>{this.props.title}</a></h3>
         <div>
         {this.props.content.replace(/(<([^>]+)>)/ig,"").substring(0,250)+"..."};
