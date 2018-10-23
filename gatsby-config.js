682e448e225fa421e62359b7b5f8272dd0d17684
Nicholas Lewis <nicklewisatx@gmail.com>
2018-10-23 09:53:37 -0400

    Updating main scripts

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,16 +14,22 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ]
+      }
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
-            
-
               ignoreFileExtensions: [],
-
             },
 
           },
