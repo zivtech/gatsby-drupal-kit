aefaf6a505dd7e4616f2bc5f5076020f1d33f3d6
Nicholas Lewis <nicklewisatx@gmail.com>
2018-11-15 20:33:44 -0500

    Further culing of the "container" component

--- src/templates/node/page/index.js
+++ src/templates/node/page/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import Img from "gatsby-image"
-import Layout from "../../../components/layouts/default"
-import NodeInfo from "../../../components/content/node-info"
-import { rhythm } from "../../../utils/typography"
-import constants from "../../../utils/constants"
-import { graphql } from 'gatsby'
+import React from "react";
+import Img from "gatsby-image";
+import Layout from "../../../components/layouts/default";
+import NodeInfo from "../../../components/content/node-info";
+import { rhythm } from "../../../utils/typography";
+import constants from "../../../utils/constants";
+import { graphql } from "gatsby";
 
 const PageTemplate = ({ data }) => (
   <Layout data={data}>
@@ -13,25 +13,20 @@ const PageTemplate = ({ data }) => (
         background: constants.paleYellow,
         padding: rhythm(1.5),
         paddingTop: 0,
-        marginBottom: rhythm(3),
+        marginBottom: rhythm(3)
       }}
     >
-      <Container>
-        <h1>{data.nodePage.title}</h1>
-        <div css={{ background: `white`, padding: rhythm(1.5) }}>
-        
-          <div css={{ display: `flex`, justifyContent: `space-between` }}>
-
-          <div dangerouslySetInnerHTML={{__html: data.nodePage.body.value }} />
-          </div>
-   
+      <h1>{data.nodePage.title}</h1>
+      <div css={{ background: `white`, padding: rhythm(1.5) }}>
+        <div css={{ display: `flex`, justifyContent: `space-between` }}>
+          <div dangerouslySetInnerHTML={{ __html: data.nodePage.body.value }} />
         </div>
-      </Container>
+      </div>
     </div>
   </Layout>
-)
+);
 
-export default PageTemplate
+export default PageTemplate;
 
 export const query = graphql`
   query($nid: Int!) {
